66c7f81cbd6d9c21869c1ec5773e8683c10da1af
Robel <robelamare75@gmail.com>
2026-04-23 18:37:07 +0300

    fix: correct sidebar toggler selector and enable toggle functionality

--- Notekeeper/src/js/app.js
+++ Notekeeper/src/js/app.js
@@ -3,3 +3,18 @@
  */
 
 'use strict';
+
+/**Module import */
+import { addEventOnElements } from "./utils";
+/**Togle sidebar in small screen */
+/**Html element */
+const $sidebar=document.querySelector('[data-sidebar]');
+/* {Array<HtmlElement}*/
+const $sidebarTogglers=document.querySelectorAll('[data-sidebar-toggler');
+/* {Array<HtmlElement}*/
+const $overlay=document.querySelector('[data-sidebar-overlay]');
+
+addEventOnElements($sidebarTogglers,'click',function(){
+    $sidebar.classList.toggle('active');
+    $overlay.classList.toggle('active');
+});
--- Notekeeper/src/js/utils.js
+++ Notekeeper/src/js/utils.js
@@ -3,3 +3,20 @@
  */
 
 'use strict';
+/**
+ * Attaches ana event listner to a collection of DOM elements
+ * @param {Array<HTMLElement>} $elements-An array of DOM elemets to attach the 
+    the event listener to
+ * @param {string} eventType -The type of event to listen for (e.g 'click')
+    ''mouseover').
+ *@param {Function} callback-the function to be executed when the event
+  occurs
+ */
+const addEventOnElements=function ($elements,eventType,callback){
+  $elements.forEach($element => $element.addEventListener(eventType,callback)
+    
+  );
+}
+export {
+    addEventOnElements
+}
